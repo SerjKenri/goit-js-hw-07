6d1aec95471e422198d48679d972fa9409787111
Sergey Kuzin <105937658+SerjKenri@users.noreply.github.com>
2022-10-15 00:02:14 +0300

    add 2 liberty

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGallery (galleryItems);
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+
+function createGallery (galleryItems) { 
+    return galleryItems.map(({preview, original, description}) => 
+        `<div class="gallery">
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" title="${description}"/>
+        </a></div>`
+).join("");
+};
+
+new SimpleLightbox('.gallery a', {
+	captionDelay: 250
+})
